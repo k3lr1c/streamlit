283923c1c10b0c238c47da97675ab5a5fe5f6b8e
k3lr1c <roberto.araya.obligado@gmail.com>
2022-07-20 22:37:29 -0400

    Update proyecto_visualizacion_de_informacion.py

--- proyecto_visualizacion_de_informacion.py
+++ proyecto_visualizacion_de_informacion.py
@@ -16,7 +16,7 @@ import altair as alt
 import pandas as pd
 import numpy as np
 import streamlit as st
-from vega_datasets import data
+import vega_datasets import data
 
 df1 = pd.read_csv('https://raw.githubusercontent.com/k3lr1c/streamlit/main/UFO_scrubbed.csv')
 
